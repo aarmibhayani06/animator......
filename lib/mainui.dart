31eb0e639647e5ef8b8a18a3cf52b771dd572c47
aarmibhayani06 <122964645+aarmibhayani06@users.noreply.github.com>
2023-12-05 01:50:17 +0530

    mainui.dart

--- lib/mainui.dart
+++ lib/mainui.dart
@@ -1,48 +1,101 @@
-import 'package:animator6/SplaceScreen.dart';
-import 'package:flutter/foundation.dart';
+import 'package:animator6/Click.dart';
+import 'package:animator6/DetailPage.dart';
+import 'package:animator6/Model.dart';
+import 'package:animator6/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences pref = await SharedPreferences.getInstance();
-  bool isDark = pref.getBool('Apptheme') ?? false;
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(
-        create: (context) => ThemeProvider(isDrak: isDark),
-      ),
-    ],
-    child: MyApp(),
-  ));
+class SlideAnimationn extends StatefulWidget {
+  @override
+  State<SlideAnimationn> createState() => _SlideAnimationnState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _SlideAnimationnState extends State<SlideAnimationn> {
+  Model model = Model(
+      name: '',
+      position: '',
+      image: '',
+      velocity: '',
+      distance: '',
+      description: '');
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      themeMode: (Provider.of<ThemeProvider>(context).isDrak)
-          ? ThemeMode.dark
-          : ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      home: MyCustomWidget(),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0),
+        child: AppBar(
+          backgroundColor: Colors.white.withOpacity(0.50),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Provider.of<ThemeProvider>(context, listen: false)
+                    .ChangeTheme();
+              },
+              icon: (Provider.of<ThemeProvider>(context).isDrak)
+                  ? Icon(Icons.dark_mode_outlined)
+                  : Icon(Icons.light_mode_outlined),
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Image(
+              image: AssetImage('assets/Backgound.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          ListView.builder(
+              padding: EdgeInsets.all(30),
+              itemCount: modelList.length,
+              itemBuilder: (context, index) {
+                Model model = modelList[index];
+                return AnimationConfiguration.staggeredList(
+                    position: index,
+                    delay: Duration(milliseconds: 300),
+                    child: FadeInAnimation(
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        duration: Duration(milliseconds: 2500),
+                        child: InkWell(
+                          onTap: () {
+                            print('Tapped on ${model.name}');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailPage(model: model),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            height: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.50),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 40,
+                                  spreadRadius: 10,
+                                ),
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                model.name,
+                              ),
+                            ),
+                          ),
+                        )));
+              }),
+        ],
+      ),
     );
-  }
-}
-
-class ThemeProvider extends ChangeNotifier {
-  bool isDrak;
-
-  ThemeProvider({required this.isDrak});
-  ChangeTheme() async {
-    isDrak = !isDrak;
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    pref.setBool("AppTheme", isDrak);
-    notifyListeners();
   }
 }
