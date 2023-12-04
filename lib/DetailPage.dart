fdfb3ec1c15fd52f6b9ea852e7e3887ae29edb92
aarmibhayani06 <122964645+aarmibhayani06@users.noreply.github.com>
2023-12-05 01:47:58 +0530

    Update DetailPage.dart

--- lib/DetailPage.dart
+++ lib/DetailPage.dart
@@ -1,6 +1,9 @@
-import 'package:animator6/Click.dart';
-import 'package:animator6/Model.dart';
+import 'package:animator6/main.dart';
 import 'package:flutter/material.dart';
+import 'package:animator6/Model.dart';
+import 'package:provider/provider.dart';
+
+// Import necessary packages and classes
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key, required this.model}) : super(key: key);
@@ -11,141 +14,117 @@ class DetailPage extends StatefulWidget {
   State<DetailPage> createState() => _DetailPageState();
 }
 
-class _DetailPageState extends State<DetailPage> {
+class _DetailPageState extends State<DetailPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    )..repeat();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 840,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 0.6,
-              color: Colors.grey.shade300,
-              blurStyle: BlurStyle.normal,
-            )
-          ],
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, top: 50, right: 20),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 0.6,
-                          color: Colors.black,
-                          blurStyle: BlurStyle.normal,
-                        )
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        widget.model.name,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        title: Text('Detail Page'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),
+          ),
+        ],
+      ),
+      body: Theme(
+        data: Theme.of(context),
+        child: Stack(
+          children: [
+            Container(
+              child: Image(
+                image: NetworkImage(
+                  'https://media.istockphoto.com/id/122147739/photo/island-moon.jpg?s=612x612&w=0&k=20&c=Lku3gve5RhfjaVngRLnC4-g6QhYDM7-Ah5Z8bbot0tc=',
+                ),
+                fit: BoxFit.fill,
+                height: double.infinity,
+                width: double.infinity,
+              ),
+            ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+                child: Column(
+                  children: [
+                    Center(
+                      child: RotationTransition(
+                        turns: _controller,
+                        child: Image.network(
+                          widget.model.image,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                      margin: EdgeInsets.only(left: 70),
-                      height: 230,
-                      width: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 0.6,
-                            blurStyle: BlurStyle.normal,
-                          )
-                        ],
-                      ),
-                      child: Column(children: [
-                        Center(
-                          child: Image.network(
-                            widget.model.image,
-                            fit: BoxFit.cover,
+                    Container(
+                      child: Center(
+                        child: Text(
+                          '${widget.model.name}',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Container(
-                            child: Text(
-                          widget.model.position,
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        )),
-                      ])),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 0.6,
-                          blurStyle: BlurStyle.normal,
-                        )
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        widget.model.velocity,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 0.6,
-                          color: Colors.black,
-                          blurStyle: BlurStyle.normal,
-                        )
-                      ],
-                    ),
-                    child: Center(
+                    SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
                       child: Text(
-                        widget.model.distance,
+                        '*Position: ${widget.model.position}',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 200,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 0.6,
-                          color: Colors.black,
-                          blurStyle: BlurStyle.normal,
-                        )
-                      ],
-                    ),
-                    child: Center(
+                    SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
                       child: Text(
-                        widget.model.description,
+                        '*Velocity: ${widget.model.velocity}',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  ),
-                ]),
-          ),
+                    SizedBox(height: 10),
+                    Container(
+                        width: double.infinity,
+                        child: Text('*Distance: ${widget.model.distance}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                    SizedBox(height: 10),
+                    Container(
+                      child: Text(
+                        '*Description: ${widget.model.description}',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
